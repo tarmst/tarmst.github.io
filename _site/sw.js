0246b75b5d71ba876238b969fe6a5b1b5d91c2e3
tarmst <thomasarmstrong174@gmail.com>
2023-04-18 13:15:57 -0400

    Updated resume, shift equivariant vision transformer project

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230417.181146';
+const cacheName = 'chirpy-20230418.131354';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
